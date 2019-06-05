46eefa3093896c175677f66eb615835b9016b259
denniarems <denniarems@gmail.com>
2019-06-05 23:28:43 +0530

    1

---
+++ 1.Re-ID/solution.py
@@ -0,0 +1,27 @@
+def isPrime(num):
+    if num == 2:
+        return True
+    elif num < 2 or not num % 2:
+        return False
+    for i in range(3, int(num ** .5 + 1), 2):
+        if not num % i:
+            return False
+    return True
+
+
+def generatePrimes(n):
+    primes = [2, ]
+    noOfPrimes = 1
+    testNum = 3
+    while noOfPrimes < n:
+        if isPrime(testNum):
+            primes.append(testNum)
+            noOfPrimes += 1
+        testNum += 2
+    return primes
+
+
+def solution(i):
+    primeList = generatePrimes(i+5)
+    minionsId = ("".join(map(str, primeList)))
+    return minionsId[i:i+5]
